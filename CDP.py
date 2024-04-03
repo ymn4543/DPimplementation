398db968e7dc2f37fd705de1ba86e86cc024ff32
ScottWu12 <165933709+ScottWu12@users.noreply.github.com>
2024-04-03 12:39:20 -0400

    With the data set addaed CDP.py

--- CDP.py
+++ CDP.py
@@ -1,6 +1,8 @@
 from pydp.algorithms.laplacian import BoundedMean
 import numpy as np
 
+url = 'https://github.com/ymn4543/DPimplementation/blob/main/healthcare_dataset%202.csv'
+def pd.read_csv(url)
 
 def main():
     print("hello world")
