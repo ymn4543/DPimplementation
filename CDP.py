2c17fe2a73e10ed9e446bbfb30779f876f805d33
ScottWu12 <165933709+ScottWu12@users.noreply.github.com>
2024-04-03 13:21:03 -0400

    Update CDP.py

--- CDP.py
+++ CDP.py
@@ -4,7 +4,7 @@ import pandas as pd
 def main():
     beta = 1
     df = pd.read_csv('healthcare_dataset.csv')
-
+    df_2 = pd.read_csv('healthcare_dataset.csv')
     noise = np.random.laplace(0, beta, df['Room Number'].size)
     df['Room Number'] += noise
     noise = np.random.laplace(0, beta, df['Billing Amount'].size)
