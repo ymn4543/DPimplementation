8abc53414ae1e95dbae91208b61f81228aeb71b3
ScottWu12 <165933709+ScottWu12@users.noreply.github.com>
2024-04-03 12:47:26 -0400

    Update w/ room number CDP.py

--- CDP.py
+++ CDP.py
@@ -4,6 +4,14 @@ import numpy as np
 url = 'https://github.com/ymn4543/DPimplementation/blob/main/healthcare_dataset%202.csv'
 def pd.read_csv(url)
 
+
+# Generate Laplace noise for the 'room number' column
+noise = np.random.laplace(0, beta, df['room number'].size)
+
+# Add noise to the original 'room number' column
+df ['ages'] += noise 
+
+
 def main():
     print("hello world")
 
