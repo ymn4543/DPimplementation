149c5fb7c88b20e973a6ff8937beb62874f3b612
Youssef naguib <ymn4543@rit.edu>
2024-04-05 12:02:42 -0400

    queries finished

--- CDP.py
+++ CDP.py
@@ -2,26 +2,10 @@ import numpy as np
 import pandas as pd
 
 def main():
-    # beta = 1
     df = pd.read_csv('healthcare_dataset.csv')
-    # noise = np.random.laplace(0, beta, df['Room Number'].size)
-    # df['Room Number'] += noise
 
-    # noise = np.random.laplace(0, beta, df['Billing Amount'].size)
-    # df['Billing Amount'] += noise
-
-    # age_noise = np.random.laplace(0, beta, df['Age'].size)
-    # df['Age'] += age_noise
-
-    # # Display the updated DataFrame
-    # print(df['Age'])
-
-    # df.to_csv('noisy_healthcare_dataset.csv', index=False)
-
-
-    # Age vs Avg billing amount for that age
-    # Define age bins starting from 18 with a step you prefer, e.g., 10-year ranges
-    bins = np.arange(18, df['Age'].max() + 10, 10)  # Adjust the step and max value as needed
+    # Age vs Average billing amount for that age
+    bins = np.arange(18, df['Age'].max() + 10, 10)
 
     # Create age ranges
     df['AgeRange'] = pd.cut(df['Age'], bins, right=False)
@@ -35,16 +19,16 @@ def main():
     print(average_billing_by_gender)
 
     # Medication vs Avg billing amount for that medication
-    average_billing_by_medication = df.groupby('Medication')['BillingAmount'].mean().reset_index()
+    average_billing_by_medication = df.groupby('Medication')['Billing Amount'].mean().reset_index()
     print(average_billing_by_medication)
 
     # Number of normal diagnosis by age
     normal_diagnoses_by_age_range = df[df['Test Results'] == 'Normal'].groupby('AgeRange').size()
     print(normal_diagnoses_by_age_range)
 
-    # Number of people who have blood type O, both + and - 
-    number_of_people_with_blood_type_O = df[df['Blood Type'].str.startswith('O')].shape[0]
-
+    # Number of people who have blood type
+    blood_type_counts = df['Blood Type'].value_counts()
+    print("Blood type counts:\n", blood_type_counts)
 
 
 main()
